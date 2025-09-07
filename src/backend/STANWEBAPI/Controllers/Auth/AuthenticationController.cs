3a2d00be674ddc905462150f6c4ad03c582b13b6
Takobz <kobelakhutso.kk@gmail.com>
2025-09-07 14:52:54 +0200

    chore: remove development settings file from src ctrl

---
+++ src/backend/STANWEBAPI/Controllers/Auth/AuthenticationController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace STANWEBAPI.Controllers.Auth;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthenticationController : ControllerBase
+{
+    
+}
